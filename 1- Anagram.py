735c05e112599301c1bda0673f9e0f1c4b4caa09
Laiba Kamal <laibakamal003@gmail.com>
2023-01-18 19:44:21 +0500

    updated Anagram.py

--- 1- Anagram.py
+++ 1- Anagram.py
@@ -1,7 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 18 12:23:15 2023
+#18/01/2023
+#242. Valid Anagram
+#(explained thoroughly in cpp solution)
+class Solution(object):
+    def isAnagram(self, s, t):
+        #Count() is a Python built-in function 
+        #that returns the number of times an 
+        #object appears in a list. It returns the 
+        #number of times a given value occurs 
+        #in a string or a list, as the name implies.
+        sMap= {
+                c:s.count(c) for c in set(s)#stores the frequency of all characters in s
+              }
+        tMap={
+                c:t.count(c) for c in set(t)#stores the frequency of all characters in t
+            }
+        
+        return sMap==tMap
+    
 
-@author: User
-"""
 
+temp=Solution()
+b=temp.isAnagram("rat", "car")
+print(b)
